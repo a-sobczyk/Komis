88c7eb2d358ad6f10e88e4ecb82e156402fdc14a
Andrzej <k0k0@tlen.pl>
2018-10-28 16:02:28 +0100

    Dodanie widoki oraz Layout'u

---
+++ Komis/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Komis.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Komis.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISamochod samochod;
+
+        public HomeController(ISamochod samochodRepository)
+        {
+            samochod = samochodRepository;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.tytul = "Przeglad samochodów";
+
+            var samochody = samochod.PobierzWszystkieSamochody().OrderBy(s => s.Marka);
+
+            return View(samochody);  
+        }
+    }
+}
